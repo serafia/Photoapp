f8b3b3eed21a0da67cef3f407f2f55b8ab958efb
Justyna Piłat <justa0904@gmail.com>
2018-01-10 19:13:07 +0100

    Modify script.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,24 @@
-$('body').scrollspy({ target: '#navbar' });
+$(function(){
+	$('body').scrollspy({ target: '#navbar' });
 
-$(window).scroll(function() {
-	changeNavbar();
+	$(window).scroll(function() {
+		changeNavbar();
+	});
+
+	function changeNavbar() {
+		var scrollPosition = $(window).scrollTop();
+		if(scrollPosition > 0) {
+			$('#navbar').addClass('dark');
+		} else {
+			$('#navbar').removeClass('dark');
+		}
+	}
+
+	$('#myCarousel1').on('slid.bs.carousel', function () {
+	  var slideNumber = $('#slideNumber');
+	  var slideActiveNumber = $('div.active').data('number');
+	  slideNumber.text(slideActiveNumber);
+	});
 });
 
-function changeNavbar() {
-	var scrollPosition = $(window).scrollTop();
-	if(scrollPosition > 0) {
-		$('#navbar').addClass('dark');
-	} else {
-		$('#navbar').removeClass('dark');
-	}
-}
-
-$('#myCarousel1').on('slid.bs.carousel', function () {
-  var slideNumber = $('#slideNumber');
-  var slideActiveNumber = $('div.active').data('number');
-  slideNumber.text(slideActiveNumber);
-})
 
